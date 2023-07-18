b854c2700270ecc5d8a0d7ffb8c955bb6b33b0e2
gabriellelailler <gabrielle.lailler@gmail.com>
2023-07-18 18:48:09 +0200

    Création des boutons de filtres (non fonctionnels)

--- Backend/script/script.js
+++ Backend/script/script.js
@@ -1,19 +1,61 @@
-const url = "http://localhost:5678/api/works"
-const container = document.getElementById("gallery")
+const containerCategories = document.getElementById("categories");
+const containerWorks = document.getElementById("gallery");
 
-const getWorks = () => {
-    fetch(url)
-    .then(function (result) {
-        return result.json()
-    })
-    .then(function (data) {
-        console.log(data)
-        for (product in data)
-        container.innerHTML += `<figure>
-        <img src="${data[product].imageUrl}" alt="${data[product].title}">
-        <figcaption>${data[product].title}</figcaption>
-    </figure>`
-    })
+
+
+// Ajout du bouton "Tous"
+// ⚠️ il faut mettre cette partie la dans la partie en dessous 
+// --> je pense qu'il faut que tout le html soit complet pour que l.event listener fonctionne
+const getCategoryTous = () => {
+    containerCategories.innerHTML += `
+    <div>
+    <button id="Button">Tous</button>
+    </div>
+    `;
+
+    let monBouton = document.getElementById("Button");
+    monBouton.addEventListener("click", () => {
+    console.log("Vous avez cliqué sur le bouton")
+    });
 }
+getCategoryTous();
 
-getWorks()
+// Récupération des Categories via API/categories + réponse au format json
+const getCategories = () => {
+    fetch("http://localhost:5678/api/categories")
+      .then(function (responseCategories) {
+        return responseCategories.json();
+      })
+      .then(function (dataCategories) {
+        console.log(dataCategories);
+        for (productCategories in dataCategories)
+        containerWorks.innerHTML += `
+        <div>
+        <button>${dataCategories[productCategories].name}</button>
+        </div>
+          `;
+      });
+  };
+  
+  getCategories();
+
+
+// Récupération des Works via API/works + réponse au format json
+const getWorks = () => {
+  fetch("http://localhost:5678/api/works")
+    .then(function (responseWorks) {
+      return responseWorks.json();
+    })
+    .then(function (dataWorks) {
+      console.log(dataWorks);
+      for (productWorks in dataWorks)
+      containerWorks.innerHTML += `
+        <figure>
+        <img src="${dataWorks[productWorks].imageUrl}" alt="${dataWorks[productWorks].title}">
+        <figcaption>${dataWorks[productWorks].title}</figcaption>
+        </figure>
+        `;
+    });
+};
+
+getWorks();
